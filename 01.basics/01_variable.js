3aa94df41b26bcc1b909da059e837790bb82af41
tushar99119911 <tushar.22scse1012576@galgotiasuniversity.edu.in>
2025-11-02 19:40:42 +0000

    just for jnowledge

---
+++ 01.basics/01_variable.js
@@ -0,0 +1,5 @@
+const accountId = 123 
+let accontEmail = "Tushar9911"
+var accontPassword = "12345"
+accontCity = "Jaipur"
+ 
---
+++ 01.basics/02_basics.js
@@ -0,0 +1,1 @@
+//alert("Tushar Is Great") Wea are using node js not browser

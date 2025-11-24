f68aee76f58704d7a6ba297b946ba5dc92f65670
tushar99119911 <tushar.22scse1012576@galgotiasuniversity.edu.in>
2025-11-24 21:28:44 +0000

    Add conversion examples for number and string types

---
+++ 01.basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = "99i";
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score);
+// console.log( typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => number
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+//let isLoggedIn = 1;
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//1 => true ; 0 => false
+// "" => false ; "abc" => true
+
+let someNumber = 75;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);

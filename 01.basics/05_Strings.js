96f71033916ea6cdcc2f84eceea8df783a14b109
tushar99119911 <tushar.22scse1012576@galgotiasuniversity.edu.in>
2025-11-29 09:38:27 +0000

    Add string manipulation examples in 05_Strings.js

---
+++ 01.basics/05_Strings.js
@@ -0,0 +1,17 @@
+// const name = "Tushar Singh Raghuvanshi"
+// const repo = 50
+// console.log(name + repo + " is the number of repositories in my GitHub profile.");
+// console.log(`${name} has ${repo} repositories in git`);
+const gameName = new String("The Witcher 3: Wild Hunt");
+// console.log(gameName);
+// console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.includes("Witcher"));
+const newString = gameName.substring(0,9);
+console.log(newString);
+
+const anotherString = gameName.slice(0,4);
+console.log(anotherString);
+

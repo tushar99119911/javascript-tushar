27ad23e47f125446e713b92b3c551419d8e2aba9
tushar99119911 <tushar.22scse1012576@galgotiasuniversity.edu.in>
2025-12-01 11:48:58 +0000

    basic array

--- 02.basics/02_array.js
+++ 02.basics/02_array.js
@@ -1,39 +1,31 @@
-// array
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
 
-const myArr = [0, 1, 2, 3, 4, 5]
-const myHeors = ["shaktiman", "naagraj"]
+// marvel_heros.push(dc_heros)
 
-const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
 
-// Array methods
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+const all_new_heros = [...marvel_heros, ...dc_heros]
 
-// myArr.unshift(9)
-// myArr.shift()
+// console.log(all_new_heros);
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-// const newArr = myArr.join()
-
-// console.log(myArr);
-// console.log( newArr);
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
 
 
-// slice, splice
 
-console.log("A ", myArr);
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
 
-const myn1 = myArr.slice(1, 3)
+let score1 = 100
+let score2 = 200
+let score3 = 300
 
-console.log(myn1);
-console.log("B ", myArr);
-
-
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+console.log(Array.of(score1, score2, score3));

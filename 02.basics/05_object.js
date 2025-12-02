3a5f8b9fe3ddc8e58a25c084cd4a0a53138f8d54
tushar99119911 <tushar.22scse1012576@galgotiasuniversity.edu.in>
2025-12-02 18:02:53 +0000

    obejct intermidia

---
+++ 02.basics/05_object.js
@@ -0,0 +1,13 @@
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}

8949089275025ed14909cc4b0d7dbdbdbeab1bdf
tushar99119911 <tushar.22scse1012576@galgotiasuniversity.edu.in>
2026-03-11 05:15:21 +0000

    revision

--- 01.basics/01_variable.js
+++ 01.basics/01_variable.js
@@ -2,4 +2,13 @@ const accountId = 123
 let accontEmail = "Tushar9911"
 var accontPassword = "12345"
 accontCity = "Jaipur"
- 
+
+console.log(accountId);
+console.table([accountId, accontEmail, accontPassword, accontCity])
+
+// accountId = 321
+accontEmail = "Tushar0909"
+accontPassword = "54321"
+accontCity = "Delhi"
+
+console.table([accountId, accontEmail, accontPassword, accontCity])
--- 01.basics/02_basics.js
+++
@@ -1,1 +0,0 @@
-//alert("Tushar Is Great") Wea are using node js not browser
